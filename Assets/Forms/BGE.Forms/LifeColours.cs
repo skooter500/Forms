c8cfecaf1284273c339c1919f34f7554ada9ea21
skooter500 <skooter500@gmail.com>
2017-09-06 23:52:43 +0100

    bla

---
+++ Assets/Forms/BGE.Forms/LifeColours.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace BGE.Forms
+{
+    public class LifeColours : MonoBehaviour
+    {
+        Renderer[] children;
+        Texture2D lifeTexture;
+        GameOfLifeTextureGenerator textureGenerator;
+
+        public float updatesPerSecond = 20.0f;
+
+        // Use this for initialization
+        void Start()
+        {
+            children = GetComponentsInChildren<Renderer>();
+
+            textureGenerator = FindObjectOfType<GameOfLifeTextureGenerator>();
+            lifeTexture = textureGenerator.texture;
+            StartCoroutine(UpdateColours());
+        }
+
+        System.Collections.IEnumerator UpdateColours()
+        {
+            yield return new WaitForSeconds(Random.Range(0.0f, 1.0f));
+            while (true)
+            {
+                foreach (Renderer child in children)
+                {
+                    RaycastHit hit;
+                    if (Physics.Raycast(child.transform.position, -Vector3.up, out hit))
+                    {
+                        Color c = lifeTexture.GetPixelBilinear(hit.textureCoord.x, hit.textureCoord.y);
+                        if (c == textureGenerator.backGround)
+                        {
+                            c.r = 1.0f - c.r;
+                            c.g = 1.0f - c.g;
+                            c.b = 1.0f - c.b;
+                        }
+                        child.material.color = Color.Lerp(child.material.color, c, 0.1f);
+                    }
+                    //yield return WaitFor.Frames(1);
+                }
+                yield return new WaitForSeconds(1.0f / updatesPerSecond);
+            }
+        }
+    }
+}
