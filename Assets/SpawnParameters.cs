cc68a350cbac0da576336536cdd4fe80c8a37545
skooter500 <skooter500@gmail.com>
2017-10-15 18:39:45 +0100

    spawn_params

---
+++ Assets/SpawnParameters.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnParameters : MonoBehaviour {
+    public float start = 3000;
+    public float end = 5000;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
