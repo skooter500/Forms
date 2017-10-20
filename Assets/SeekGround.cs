8f8ee088217cd1d3afda6a89d05738e131e818ed
Bryan Duggan <skooter500@gmail.com>
2017-10-21 00:38:17 +0100

    bla

---
+++ Assets/SeekGround.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BGE.Forms
+{
+    public class SeekGround : Behaviour
+    {
+
+        public float height;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Calculate()
+        {
+
+        }
+    }
+}
