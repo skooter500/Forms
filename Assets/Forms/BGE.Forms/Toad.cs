887605451b00359c247b3aa1c5767728de9bfbbe
Bryan Duggan <skooter500@gmail.com>
2017-12-02 21:33:58 +0000

    bla

---
+++ Assets/Forms/BGE.Forms/Toad.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BGE.Forms
+{
+    public class Toad : MonoBehaviour
+    {
+        Camera camera;
+
+        // Use this for initialization
+        void Start()
+        {
+            camera = Camera.main;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void OnTriggerStay()
+        {
+            camera.fieldOfView = Mathf.Lerp(camera.fieldOfView, 180, Time.deltaTime);
+        }
+    }
+}
