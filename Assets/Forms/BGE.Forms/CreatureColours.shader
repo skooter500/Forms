50fe3b219e908dad81f129f6381f03ce8c31bdf7
Bryan Duggan <skooter500@gmail.com>
2018-01-23 00:34:58 +0000

    Beautiful

--- Assets/Forms/BGE.Forms/CreatureColours.shader
+++ Assets/Forms/BGE.Forms/CreatureColours.shader
@@ -1,9 +1,9 @@
 ﻿Shader "Custom/CreatureColours" {
 	Properties {
-		_Color ("Color", Color) = (1,1,1,1)
 		_MainTex ("Albedo (RGB)", 2D) = "white" {}
 		_Glossiness ("Smoothness", Range(0,1)) = 0.5
 		_Metallic ("Metallic", Range(0,1)) = 0.0
+		_PositionScale("PositionScale", Range(0, 1000)) = 250
 	}
 	SubShader {
 		Tags { "RenderType"="Opaque" }
@@ -20,11 +20,13 @@
 
 		struct Input {
 			float2 uv_MainTex;
+			float3 worldPos;
 		};
 
 		half _Glossiness;
 		half _Metallic;
-		fixed4 _Color;
+
+		float _PositionScale;
 
 		// Add instancing support for this shader. You need to check 'Enable Instancing' on materials that use the shader.
 		// See https://docs.unity3d.com/Manual/GPUInstancing.html for more information about instancing.
@@ -35,7 +37,13 @@
 
 		void surf (Input IN, inout SurfaceOutputStandard o) {
 			// Albedo comes from a texture tinted by color
-			fixed4 c = tex2D (_MainTex, IN.uv_MainTex);
+			float u,v;			
+			u = abs(IN.worldPos.x / _PositionScale);
+			//u -= (int)u;
+            v = abs(IN.worldPos.z / _PositionScale);
+			//v -= (int)v;
+			fixed4 c = tex2D (_MainTex, float2(u,v));
+
 			o.Albedo = c.rgb;
 			// Metallic and smoothness come from slider variables
 			o.Metallic = _Metallic;
--- Assets/Forms/BGE.Forms/LifeColours.cs
+++ Assets/Forms/BGE.Forms/LifeColours.cs
@@ -7,183 +7,51 @@ namespace BGE.Forms
 {
     public class LifeColours : MonoBehaviour
     {
+        public Material material;
         Renderer[] children;
-        Texture2D lifeTexture;
-        GameOfLifeTextureGenerator textureGenerator;
+        int size = 256;
 
-        public float updatesPerSecond = 20.0f;
-
-        Color[,] colorGrid;
-        int size;
-
-        public float colorMapScaling = 250;
+        public float colorMapScaling = 50;
 
         public Texture2D texture;
         public float colorScale = 0.7f;
 
+        void OnValidate()
+        {
+        }
+
         // Use this for initialization
         void Start()
         {
             children = GetComponentsInChildren<Renderer>();
-
-            textureGenerator = FindObjectOfType<GameOfLifeTextureGenerator>();
-            lifeTexture = textureGenerator.texture;
-
-            size = textureGenerator.size;
-            colorGrid = new Color[size, size];
             texture = new Texture2D(size, size);
-            
+
+            int halfSize = size / 2;
             for (int row = 0; row < size; row++)
             {
                 for (int col = 0; col < size; col++)
                 {
-                    float hue = (row / (float) size * colorScale) + (col / (float) size * colorScale) / 2.0f;
-                    colorGrid[row, col] = Color.HSVToRGB(hue, 1, 0.8f);
-
-                    texture.SetPixel(row, col, colorGrid[row, col]);
-                }
-            }
-            texture.Apply();            
-        }
-
-        private void OnEnable()
-        {
-            StartCoroutine(UpdateColours());
-        }
-
-        System.Collections.IEnumerator UpdateColours()
-        {
-            yield return new WaitForSeconds(Random.Range(0.0f, 1.0f));
-            while (true)
-            {
-                foreach (Renderer child in children)
-                {
-                    if (child.material.color.a != 1.0f)
-                    {
-                        continue;
-                    }
-
-                    /*
-                    int col = ((int)(Mathf.Abs(child.transform.position.x / colorMapScaling))) % size;
-                    int row = ((int)(Mathf.Abs(child.transform.position.z / colorMapScaling))) % size;
-
-                    Color c; = colorGrid[row, col];
+                    float hue = (row / (float)size * colorScale) + (col / (float)size * colorScale) / 2.0f;
+                    /*float hue = (col < halfSize)
+                        ? Utilities.Map(col, 0, halfSize -1, 0.01f, colorScale)
+                        : Utilities.Map(col, halfSize, size -1, colorScale, 0.01f);
                     */
-
-
-                    int col = ((int)(Mathf.Abs(child.transform.position.x / colorMapScaling))) % size;
-                    int row = ((int)(Mathf.Abs(child.transform.position.z / colorMapScaling))) % size;
-
-                    Color c = colorGrid[row, col];
-                    child.material.color = Color.Lerp(child.material.color, c, 0.01f);
-
-                    /*
-                    RaycastHit hit;
-                    if (Physics.Raycast(child.transform.position, -Vector3.up, out hit))
-                    {
-
-                        Color c = Color.black;
-                        int col = ((int)(Mathf.Abs(child.transform.position.x / 200))) % size;
-                        int row = ((int)(Mathf.Abs(child.transform.position.z / 200))) % size;
-
-
-                        //c = lifeTexture.GetPixelBilinear(hit.textureCoord.x, hit.textureCoord.y);
-                        if (c == textureGenerator.backGround)
-                        {
-                            c = colorGrid[row, col];
-                            child.material.color = Color.Lerp(child.material.color, c, 0.01f);
-                        }
-                        else
-                        {
-                            c =  lifeTexture.GetPixelBilinear(hit.textureCoord.x, hit.textureCoord.y);
-                            child.material.color = Color.Lerp(child.material.color, c, 0.1f);
-
-                        }
-                    }     
-                    */               
-                    //yield return WaitFor.Frames(1);
+                    texture.SetPixel(row, col, Color.HSVToRGB(hue, 1, 0.8f));
                 }
-                yield return new WaitForSeconds(1.0f / updatesPerSecond);
+            }
+            texture.Apply();
+            texture.wrapMode = TextureWrapMode.Mirror;
+
+            foreach (Renderer child in children)
+            {
+                if (child.material.name.Contains("Trans"))
+                {
+                    continue;
+                }
+                child.material = material;
+                child.material.SetFloat("_PositionScale", colorMapScaling);
+                child.material.mainTexture = texture;                
             }
         }
     }
 }
-
-
-
-/*using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-
-namespace BGE.Forms
-{
-    public class LifeColours : MonoBehaviour
-    {
-        Renderer[] children;
-        Texture2D lifeTexture;
-        GameOfLifeTextureGenerator textureGenerator;
-
-        Color[,] colourGrid;
-
-        public float updatesPerSecond = 20.0f;
-        int size;
-        // Use this for initialization
-        void Start()
-        {
-            children = GetComponentsInChildren<Renderer>();
-
-            textureGenerator = FindObjectOfType<GameOfLifeTextureGenerator>();
-            lifeTexture = textureGenerator.texture;
-            size = textureGenerator.size;
-            colourGrid = new Color[size, size];
-            for (int row = 0; row < size; row++)
-            {
-                for (int col = 0; col < size; col++)
-                {
-                    colourGrid[row, col] = textureGenerator.RandomColor();
-                }
-            }
-
-            StartCoroutine(UpdateColours());
-        }
-
-        System.Collections.IEnumerator UpdateColours()
-        {
-            yield return new WaitForSeconds(Random.Range(0.0f, 1.0f));
-            while (true)
-            {
-                foreach (Renderer child in children)
-                {
-                    if (child.material.color.a != 1.0f)
-                    {
-                        continue;
-                    }
-
-                    RaycastHit hit;
-                    if (Physics.Raycast(child.transform.position, -Vector3.up, out hit))
-                    {
-                        Color c = lifeTexture.GetPixelBilinear(hit.textureCoord.x, hit.textureCoord.y);
-
-                        //int col = ((int)(Mathf.Abs(child.transform.position.x))) % size;
-                        //int row = ((int)(Mathf.Abs(child.transform.position.z))) % size;
-
-                        //child.material.color = Color.Lerp(child.material.color, colourGrid[row, col], 0.1f);
-
-                        if (c == textureGenerator.backGround)
-                        {
-                            c.r = 1.0f - c.r;
-                            c.g = 1.0f - c.g;
-                            c.b = 1.0f - c.b;
-                        }
-                        child.material.color = Color.Lerp(child.material.color, c, 0.1f);
-                    }
-                    
-                    //yield return WaitFor.Frames(1);
-                    yield return new WaitForSeconds(1.0f / updatesPerSecond);
-                }
-            }
-        }
-    }
-}
-*/
