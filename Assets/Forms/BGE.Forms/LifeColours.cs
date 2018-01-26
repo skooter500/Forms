28c3bcbd096b87e9bd6dbd1c46d1b82f70f46afa
Bryan Duggan <skooter500@gmail.com>
2018-01-26 00:28:00 +0000

    Different in shader gen

--- Assets/Forms/BGE.Forms/LifeColours.cs
+++ Assets/Forms/BGE.Forms/LifeColours.cs
@@ -7,36 +7,78 @@ namespace BGE.Forms
 {
     public class LifeColours : MonoBehaviour
     {
+        public Material creatureTextureMaker;
         public Material material;
         Renderer[] children;
         int size = 256;
 
         public float colorMapScaling = 50;
 
-        public Texture2D texture;
+        public Texture texture;
         public float colorScale = 0.7f;
 
-        void OnValidate()
+        //public RenderTexture texture;
+        private RenderTexture buffer;
+
+        public enum TextureMode { Shader, CSharp }
+
+        public TextureMode textureMode = TextureMode.Shader;
+
+        public void UpdateTexture()
         {
+            Graphics.Blit(texture, buffer, material);
+            Graphics.Blit(buffer, (RenderTexture)texture);
         }
 
-        // Use this for initialization
-        void Start()
+        private void InitializeShaderTexture()
         {
-            children = GetComponentsInChildren<Renderer>();
-            texture = new Texture2D(size, size);
+            RenderTexture renderTexture = (RenderTexture)texture;
+            buffer = new RenderTexture(renderTexture.width
+                , renderTexture.height
+                , renderTexture.depth
+                , renderTexture.format
+                );
+
+            // Run the shader to generate the texture
+            creatureTextureMaker.SetFloat("_ColourScale", colorScale);
+            Graphics.Blit(texture, buffer, creatureTextureMaker);
+            Graphics.Blit(buffer, renderTexture);
+        }
+
+        private void InitializeProgrammableTexture()
+        {
+            Texture2D programmableTexture = new Texture2D(size, size);
+            texture = programmableTexture;
 
             int halfSize = size / 2;
             for (int row = 0; row < size; row++)
             {
                 for (int col = 0; col < size; col++)
                 {
-                    float hue = ((row / (float)size) * colorScale) + ((col / (float)size) * colorScale) / 2.0f;
-                    texture.SetPixel(row, col, Color.HSVToRGB(hue, 1, 0.8f));
+                    float hue = Utilities.Map(row + col, 0, (size * 2) - 2, 0, colorScale);
+                    // ((row / (float)size) * colorScale) + ((col / (float)size) * colorScale) / 2.0f;
+                    programmableTexture.SetPixel(row, col, Color.HSVToRGB(hue, 1, 0.8f));
                 }
             }
-            texture.Apply();
+            programmableTexture.Apply();
             texture.wrapMode = TextureWrapMode.Mirror;
+
+        }
+
+
+        // Use this for initialization
+        void Start()
+        {
+            children = GetComponentsInChildren<Renderer>();
+
+            if (textureMode == TextureMode.Shader)
+            {
+                InitializeShaderTexture();
+            }
+            else
+            {
+                InitializeProgrammableTexture();
+            }
 
             foreach (Renderer child in children)
             {
@@ -46,7 +88,7 @@ namespace BGE.Forms
                 }
                 child.material = material;
                 child.material.SetFloat("_PositionScale", colorMapScaling);
-                child.material.mainTexture = texture;                
+                child.material.mainTexture = texture;
             }
         }
     }
---
+++ Assets/Forms/Textures/CreatureTextureMaker.shader
@@ -0,0 +1,56 @@
+﻿// AlanZucconi.com: http://www.alanzucconi.com/?p=4539
+Shader "Custom/CreatureTextureMaker"
+{
+	Properties
+	{
+		_MainTex("Base (RGB)", 2D) = "white" {}
+		_ColourScale("Colour Scale", Range(0, 1)) = 1
+	}
+	
+	SubShader
+	{
+		// Required to work
+		ZTest Always Cull Off ZWrite Off
+		Fog{ Mode off }
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert_img
+			#pragma fragment frag
+
+			#include "UnityCG.cginc"
+
+			uniform sampler2D _MainTex;
+
+			float _ColourScale;
+
+			float3 hsvToRgb(float3 HSV)
+			{
+				float3 RGB = HSV.z;
+		   
+				float var_h = HSV.x * 6;
+				float var_i = floor(var_h);   // Or ... var_i = floor( var_h )
+				float var_1 = HSV.z * (1.0 - HSV.y);
+				float var_2 = HSV.z * (1.0 - HSV.y * (var_h-var_i));
+				float var_3 = HSV.z * (1.0 - HSV.y * (1-(var_h-var_i)));
+				if      (var_i == 0) { RGB = float3(HSV.z, var_3, var_1); }
+				else if (var_i == 1) { RGB = float3(var_2, HSV.z, var_1); }
+				else if (var_i == 2) { RGB = float3(var_1, HSV.z, var_3); }
+				else if (var_i == 3) { RGB = float3(var_1, var_2, HSV.z); }
+				else if (var_i == 4) { RGB = float3(var_3, var_1, HSV.z); }
+				else                 { RGB = float3(HSV.z, var_1, var_2); }
+		   
+				return (RGB);
+			}
+
+			float4 frag(v2f_img i) : COLOR
+			{
+				float h = ((i.uv.x + i.uv.y) / 2.0f) * _ColourScale;
+				float3 c = hsvToRgb(float3(h, 1, 0.8f));
+				return float4(c, 1);
+			}
+			ENDCG
+		}
+	}
+}
