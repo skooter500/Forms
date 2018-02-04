686acaba18e3f0aec33e85aaabd3b7361b395ae4
Bryan Duggan <skooter500@gmail.com>
2018-02-04 13:10:17 +0000

    wip

---
+++ Assets/TardigradeSchoolController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BGE.Forms;
+
+public class TardigradeSchoolController : MonoBehaviour {
+
+    Boid[] boids;
+
+    public class IdleState : State
+    {
+        public override void Enter() { }
+        public override void Exit() { }
+
+    }
+
+    // Use this for initialization
+    void Start () {
+        boids = GetComponentsInChildren<Boid>();
+        GetComponent<StateMachine>().ChangeState(new IdleState());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
