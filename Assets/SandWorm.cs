fab3ff8e40a0fa71155ff578184fa9dd7148d9b3
skooter500 <skooter500@gmail.com>
2018-08-10 20:45:49 +0100

    fixed audio reactive colours

---
+++ Assets/SandWorm.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SandWorm : MonoBehaviour {
+    public int bodySegments = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void Instiantiate()
+    {
+        for (int i = 0; i < bodySegments; i++)
+        {
+
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
