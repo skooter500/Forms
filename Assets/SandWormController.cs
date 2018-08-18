f32dd7858a34ac535f1e4a7164b389947c8298b1
Bryan Duggan <skooter500@gmail.com>
2018-08-18 03:13:16 +0100

    bla

--- Assets/SandWormController.cs
+++ Assets/SandWormController.cs
@@ -4,25 +4,34 @@ using UnityEngine;
 
 public class SandWormController : CreatureController
 {
+    public float distanceToPlayer = 180.0f;
+
     System.Collections.IEnumerator Control()
     {
+
         GetComponent<SandWorm>().moving = false;
         yield return new WaitForSeconds(10);
+        Transform sw = transform.GetChild(transform.childCount -1);
+        Transform player = GameObject.FindGameObjectWithTag("Player").transform;
+        GetComponent<SandWorm>().moving = true;
         while (true)
-        {
-            GetComponent<SandWorm>().moving = true;
-            yield return new WaitForSeconds(Random.Range(10, 20));
-            GetComponent<SandWorm>().moving = false;
-            yield return new WaitForSeconds(10);
+        {            
+            if (Vector3.Distance(sw.position, player.position) < distanceToPlayer)
+                GetComponent<SandWorm>().moving = false;
+            else
+                GetComponent<SandWorm>().moving = true;
+
+            yield return new WaitForSeconds(2);
+            
         }
     }
 
     public override void Restart()
     {
         GetComponent<SandWorm>().Restart();
-        //StartCoroutine(Control());
-        GetComponent<SandWorm>().moving = false;
-        Invoke("StartMoving", 10);
+        StartCoroutine(Control());
+        //GetComponent<SandWorm>().moving = false;
+        //Invoke("StartMoving", 10);
     }
 
     public void StartMoving()
