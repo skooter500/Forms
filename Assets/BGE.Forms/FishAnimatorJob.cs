06576bb22fc79256f3ecb80e805141cdfb33b75e
skooter500 <skooter500@gmail.com>
2018-09-04 15:47:11 +0100

    start prep for paradiso

---
+++ Assets/BGE.Forms/FishAnimatorJob.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Jobs;
+using UnityEngine;
+using UnityEngine.Jobs;
+using BGE.Forms;
+
+public struct FishAnimatorJob : IJobParallelForTransform
+{
+    float deltaTime;
+    float theta;
+    float angle;
+
+    public void Execute(int index, TransformAccess t)
+    {
+
+    }
+}
