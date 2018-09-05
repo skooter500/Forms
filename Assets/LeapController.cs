54780a10fa05ebde210780f8d98c104327d1f6d5
Bryan Duggan <skooter500@gmail.com>
2018-09-05 15:30:23 +0100

    bla

---
+++ Assets/LeapController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap.Unity;
+using Leap;
+using BGE.Forms;
+
+public class LeapController : MonoBehaviour {
+    public LeapProvider lp;
+	// Use this for initialization
+	void Start () {
+        lp = FindObjectOfType<LeapProvider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Frame frame = lp.CurrentFrame;
+        List<Hand> hands = frame.Hands;
+        foreach (Hand h in hands)
+        {
+            CreatureManager.Log("Palm Position" + h.PalmPosition);
+        }
+    }
+}
