bc3fa6ed8822868c0a805902c67428e879d5cc38
Bryan Duggan <skooter500@gmail.com>
2018-09-06 20:57:27 +0100

    Jelly rotation fix

--- Assets/ControllableJellyController.cs
+++ Assets/ControllableJellyController.cs
@@ -1,13 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using BGE.Forms;
 
-public class ControllableJellyController : MonoBehaviour {
+public class ControllableJellyController : CreatureController
+ {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    Boid boid;
+
+    public override void Restart()
+    {
+        Vector3 newF = Random.insideUnitCircle;
+        newF.z = newF.y;
+        newF.y = 0;
+        boid.transform.forward = newF;
+        boid.UpdateLocalFromTransform();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        boid = Utilities.FindBoidInHierarchy(this.gameObject);
+        Restart();
+    }
 	
 	// Update is called once per frame
 	void Update () {
