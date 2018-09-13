9d06fa04810fe5a5039d27348e459f48462405ed
skooter500 <skooter500@gmail.com>
2018-09-13 11:05:56 +0100

    progress

--- Assets/BGE.Forms/FishAnimatorJob.cs
+++ Assets/BGE.Forms/FishAnimatorJob.cs
@@ -9,23 +9,21 @@ public struct FishAnimatorJob : IJobParallelForTransform
     [ReadOnly]
     public NativeArray<float> theta;
 
-    float deltaTime;
-    float speedMultiplier;
-    float frequency;
-    float speed;
+    [ReadOnly]
+    public NativeArray<float> angle;
 
-    public float headField;
-    public float tailField;
-
+    public float deltaTime;
+    public float frequency;
+    public float speed;
 
     public void Execute(int i, TransformAccess t)
     {
-        float rot = (i < theta.Length / 2)
-            ? Mathf.Sin(theta[i]) * headField
-            : Mathf.Sin(theta[i]) * tailField;
+        float rot = Mathf.Sin(theta[i]) * angle[i];
         t.localRotation = Quaternion.AngleAxis(rot, Vector3.up);
         theta[i] += speed * frequency * deltaTime * Mathf.PI * 2.0f;
     }
+
+    
 }
 
 public class FishAnimatorManager : MonoBehaviour
@@ -35,16 +33,36 @@ public class FishAnimatorManager : MonoBehaviour
     JobHandle jh;
 
     NativeArray<float> theta;
+    NativeArray<float> angle;
+
+    int maxJobs = 5000;
+    int numJobs = 0;
 
     void Start()
     {
-        theta = new NativeArray<float>(1000, Allocator.Persistent);
+        theta = new NativeArray<float>(maxJobs, Allocator.Persistent);
+        angle = new NativeArray<float>(maxJobs, Allocator.Persistent);
+        transforms = new TransformAccessArray(maxJobs);
     }
 
     private void OnDisable()
     {
         jh.Complete();
         transforms.Dispose();
+    }
+
+    public void LateUpdate()
+    {
+        jh.Complete();
+    }
+
+    public void Update()
+    {
+        job = new FishAnimatorJob()
+        {
+            deltaTime = Time.deltaTime;
+            
+        }
     }
 }
 
